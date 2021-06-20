96f819629203e2513caf35bceb6b818ff42143df
alfianfadhil <alvian.wadada@gmail.com>
2021-06-20 16:32:18 +0700

    membuat structure folder

---
+++ plabstore/src/configs/router.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { routes } from "./routes";
+
+function router() {
+  return (
+    <Router>
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            {...route}
+            component={route.component}
+            exact
+            path={route.path()}
+          />
+        ))}
+      </Switch>
+    </Router>
+  );
+}
+
+export default router;
